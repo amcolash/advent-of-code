02a2e1ffc4e9adaa9975086586858608c38bebce
Andrew McOlash <amcolash@gmail.com>
2024-12-10 12:03:02 -0800

    day 10, make a new vector class

---
+++ 2024/src/10/part2.ts
@@ -0,0 +1,63 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { Point } from '../util/types';
+
+const data = readFileSync(join(__dirname, 'input.txt')).toString();
+const lines = data.trim().split('\n');
+
+const map: number[][] = [];
+const dims: Point = { x: 0, y: 0 };
+const peaks: Point[] = [];
+const trailheads: Point[] = [];
+
+lines.forEach((l, y) => {
+  map[y] = [];
+  l.split('').forEach((c, x) => {
+    const value = Number(c);
+    map[y][x] = value;
+
+    if (value === 0) trailheads.push({ x, y });
+    if (value === 9) peaks.push({ x, y });
+  });
+
+  // console.log(map[y]);
+});
+
+dims.x = map[0].length;
+dims.y = map.length;
+
+function insideBounds(point: Point) {
+  return point.x >= 0 && point.x < dims.x && point.y >= 0 && point.y < dims.y;
+}
+
+function findTrail(trailhead: Point, point: Point) {
+  const value = map[point.y][point.x];
+
+  if (value === 9) {
+    score++;
+    return;
+  }
+
+  const next = value + 1;
+
+  const up: Point = { x: point.x, y: point.y - 1 };
+  const down: Point = { x: point.x, y: point.y + 1 };
+  const left: Point = { x: point.x - 1, y: point.y };
+  const right: Point = { x: point.x + 1, y: point.y };
+
+  if (insideBounds(up) && map[up.y][up.x] === next) findTrail(trailhead, up);
+  if (insideBounds(down) && map[down.y][down.x] === next)
+    findTrail(trailhead, down);
+  if (insideBounds(left) && map[left.y][left.x] === next)
+    findTrail(trailhead, left);
+  if (insideBounds(right) && map[right.y][right.x] === next)
+    findTrail(trailhead, right);
+}
+
+let score = 0;
+
+trailheads.forEach((p) => {
+  findTrail(p, p);
+});
+
+console.log(score);
--- 2024/src/util/types.ts
+++ 2024/src/util/types.ts
@@ -2,3 +2,59 @@ export type Point = {
   x: number;
   y: number;
 };
+
+export class Vector {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  /** Construct a vector from a point */
+  static fromPoint(point: Point) {
+    return new Vector(point.x, point.y);
+  }
+
+  /** Adds vectors (modifies the original vector) */
+  add(vec: Vector | Point): Vector {
+    this.x += vec.x;
+    this.y += vec.y;
+
+    return this;
+  }
+
+  /** Subtracts vectors (modifies the original vector) */
+  sub(vec: Vector | Point): Vector {
+    this.x -= vec.x;
+    this.y -= vec.y;
+
+    return this;
+  }
+
+  /** Multiplies vectors (modifies the original vector) */
+  mul(vec: Vector | Point): Vector {
+    this.x *= vec.x;
+    this.y *= vec.y;
+
+    return this;
+  }
+
+  /** Divides vectors (modifies the original vector) */
+  div(vec: Vector | Point): Vector {
+    this.x /= vec.x;
+    this.y /= vec.y;
+
+    return this;
+  }
+
+  /** Clone the vector - usually before use in a math operation */
+  clone(): Vector {
+    return new Vector(this.x, this.y);
+  }
+
+  toString() {
+    return `[${this.x}, ${this.y}]`;
+  }
+}
