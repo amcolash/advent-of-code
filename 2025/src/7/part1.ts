4fd5c264504887df049713ee551fca579e22b247
Andrew McOlash <amcolash@gmail.com>
2025-12-26 13:57:51 -0700

    day 7 part 1

---
+++ 2025/src/7/part1.ts
@@ -0,0 +1,43 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { buildStringGrid, printGrid } from '../util/utils';
+
+const data = readFileSync(join(__dirname, 'input.txt')).toString();
+const lines = data.trim().split('\n');
+
+const grid = buildStringGrid(lines);
+
+const width = grid[0].length;
+const height = grid.length;
+
+const START = 'S';
+const EMPTY = '.';
+const SPLIT = '^';
+const BEAM = '|';
+
+let splits = 0;
+function update(y) {
+  for (let x = 0; x < width; x++) {
+    if (grid[y][x] === START) grid[y + 1][x] = BEAM;
+    if (grid[y][x] === BEAM) {
+      if (grid[y + 1][x] === SPLIT) {
+        grid[y + 1][x - 1] = BEAM;
+        grid[y + 1][x + 1] = BEAM;
+
+        splits++;
+      } else {
+        grid[y + 1][x] = BEAM;
+      }
+    }
+  }
+}
+
+// printGrid(grid);
+// console.log();
+for (let y = 0; y < height - 1; y++) {
+  update(y);
+  // printGrid(grid);
+  // console.log();
+}
+
+console.log(splits);
---
+++ 2025/src/7/part2.ts
@@ -0,0 +1,45 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { buildStringGrid, printGrid } from '../util/utils';
+
+const data = readFileSync(join(__dirname, 'sample1.txt')).toString();
+const lines = data.trim().split('\n');
+
+const grid = buildStringGrid(lines);
+
+const width = grid[0].length;
+const height = grid.length;
+
+const START = 'S';
+const EMPTY = '.';
+const SPLIT = '^';
+const BEAM = '|';
+
+const levels = [];
+
+function update(y) {
+  for (let x = 0; x < width; x++) {
+    if (grid[y][x] === START) grid[y + 1][x] = BEAM;
+    if (grid[y][x] === BEAM) {
+      if (grid[y + 1][x] === SPLIT) {
+        grid[y + 1][x - 1] = BEAM;
+        grid[y + 1][x + 1] = BEAM;
+
+        levels[y] = levels[y] || 0;
+        levels[y]++;
+      } else {
+        grid[y + 1][x] = BEAM;
+      }
+    }
+  }
+}
+
+// printGrid(grid);
+// console.log();
+for (let y = 0; y < height - 1; y++) {
+  update(y);
+  // printGrid(grid);
+  // console.log();
+}
+
+console.log(levels.map((l, i) => `${i}: ${l}`).join('\n'));
